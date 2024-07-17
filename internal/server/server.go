23fae2bc1ed128d1f32ec9114d8300f4c1408ca4
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-07-18 02:34:04 +0300

    Add "GET /health"

--- internal/server/server.go
+++ internal/server/server.go
@@ -7,9 +7,22 @@ import (
 )
 
 func New(cfg *Config) *http.Server {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /health", handleGetHealth)
+
 	return &http.Server{
 		Addr:              net.JoinHostPort(cfg.Host, strconv.Itoa(cfg.Port)),
-		Handler:           http.NewServeMux(),
+		Handler:           mux,
 		ReadHeaderTimeout: cfg.ReadHeaderTimeout,
+	}
+}
+
+func handleGetHealth(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	_, err := w.Write([]byte(`{"status":"ok"}`))
+	if err != nil {
+		panic(err)
 	}
 }
